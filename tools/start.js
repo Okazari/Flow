1f8fe5546c839d86d84051bfc962edc08cefc237
unknown <Okazari>
2016-03-27 16:20:22 +0200

    Setting up industrialisation

---
+++ tools/start.js
@@ -0,0 +1,11 @@
+const DEBUG = !process.argv.includes('release');
+
+/**
+ * Launches a development web server with "live reload" functionality -
+ * synchronizing URLs, interactions and code changes across multiple devices.
+ */
+function start() {
+  console.log('coucou');
+}
+
+export default start;
