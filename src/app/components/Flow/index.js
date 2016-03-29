1be5d78b7d87bdf995f97e3d50496e314bd3fbf8
Benjamin Plouzennec <benjamin.plouzennec@zenika.com>
2016-03-29 15:59:22 +0200

    Added a removeStone function

--- src/app/components/Flow/index.js
+++ src/app/components/Flow/index.js
@@ -4,22 +4,23 @@ import FlowStone from "../FlowStone";
 import './index.scss';
 
 export default class Flow extends Component {
-    
+
     constructor(){
         super();
-        this.startMusic();
+        //this.startMusic();
+        this.directions = ['LEFT','DOWN','RIGHT','UP']
         this.state = {
             gameMap : this.getRandomMap(),
             selectedStone: {}
         }
     }
-    
-    findStone(map,x,y){ 
-        return map.filter(function(element){
+
+    findStone(map,x,y){
+        return map.filter(element => {
             return x === element.x && y === element.y;
         })[0];
     }
-    
+
     startMusic(){
         let rythm = new Rythm();
         rythm.setMusic('rythm.mp3');
@@ -33,27 +34,34 @@ export default class Flow extends Component {
         rythm.addRythm('stone-4','size',0,600);
         rythm.start();
     }
-    
+
+    getRandomStoneValue(){
+      return parseInt((Math.random()*4)+1)
+    }
+
     getRandomMap() {
         let randomMap = [];
-        let size = parseInt(3+(Math.random()*3)+1);
+        let size = 6;
         for(let x = 0; x < size; x++){
             for(let y = 0; y < size; y++){
                 randomMap.push({
                     x:x,
                     y:y,
-                    value: parseInt((Math.random()*4)+1)
+                    value: this.getRandomStoneValue()
                 });
             }
-            
+
         }
         return randomMap;
     }
-    
+
+    getMatchs(map,stone, direction){
+      map.forEach(stone => {
+
+      })
+    }
+
     swapStone(gameMap, stone1, stone2){
-        let newMap = gameMap.map(function(column, columnIndex){
-            return column;
-        });
         let tmpX = stone1.x;
         let tmpY = stone1.y;
         stone1.x = stone2.x;
@@ -62,11 +70,32 @@ export default class Flow extends Component {
         stone2.y = tmpY;
         return gameMap;
     }
-    
-    
-    
+
+
+
     render() {
-        
+
+        const removeStone = (e,stone) => {
+            let map = this.state.gameMap;
+            delete map[map.indexOf(stone)];
+            map.push({x:stone.x,
+                      y:-1,
+                      value:this.getRandomStoneValue()});
+            this.setState({
+              gameMap:map,
+            });
+            setTimeout(()=>{
+                map.forEach(otherStone =>{
+                if(otherStone.x === stone.x && otherStone.y < stone.y){
+                  otherStone.y++;
+                }
+              })
+              this.setState({
+                gameMap:map,
+              });
+            }, 10);
+        }
+
         const selectStone = (event, stone) => {
             if(this.state.selectedStone.value){
                 let newGameMap = this.state.gameMap;
@@ -80,23 +109,23 @@ export default class Flow extends Component {
             }else{
                 this.setState({
                     selectedStone:stone
-                })    
+                })
             }
         }
-        
+
         const renderStone = (stone, key) => {
             return (
-                <FlowStone key={key} stone={stone} onSelect={(e,x,y,value) => selectStone(e, stone)}>
+                <FlowStone key={key} stone={stone} onSelect={(e,x,y,value) => removeStone(e, stone)}>
                 </FlowStone>
             )
-        }  
-        
+        }
+
         const randomize = () => {
             this.setState({
                 gameMap : this.getRandomMap()
             })
         };
-        
+
         return (
             <div>
                 <div className="flow-container">
@@ -105,5 +134,5 @@ export default class Flow extends Component {
             </div>
         );
     }
-    
+
 }
